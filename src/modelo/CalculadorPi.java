195a5336154ee711120831a2e868ab3b6c1e9b52
Gabriel Cruz Libreros <gabrielcl45@gmail.com>
2021-05-29 17:17:23 -0500

    Add methods to the class that calculates if a point lands inside the circle

--- src/modelo/CalculadorPi.java
+++ src/modelo/CalculadorPi.java
@@ -1,80 +1,31 @@
 package modelo;
 
-import java.io.Serializable;
+import java.util.Random;
 
 public class CalculadorPi {
-	
-	private String tipo;
-	private int cantidad, minimo, critico, recarga;
-		
-	public CalculadorPi(String tipo, int cantidad, int minimo, int critico, int recarga) {
-		super();
-		this.tipo = tipo;
-		this.cantidad = cantidad;
-		this.minimo = minimo;
-		this.critico = critico;
-		this.recarga = recarga;
+
+	private final int semilla;
+	private final int puntosTotal;
+
+	public CalculadorPi(int semilla, int puntosTotal) {
+		super();//TODO: Es necesario?
+		this.semilla = semilla;
+		this.puntosTotal = puntosTotal;
 	}
 	
-	
-	
+	public int calcularPuntosAdentro(){
+		int puntosAdentro = 0;
 
-	public int getRecarga() {
-		return recarga;
+		Random rng = new Random(semilla);
+		float currentX = rng.nextFloat();
+		float currentY = rng.nextFloat();
+		float position = (currentX * currentX) + (currentY * currentY);
+
+		for (int i = 0; i < puntosTotal; i++) {
+			if (position <= 1) {
+				puntosAdentro +=1;
+			}
+		}
+		return puntosAdentro;
 	}
-
-
-
-
-	public void setRecarga(int recarga) {
-		this.recarga = recarga;
-	}
-
-
-
-
-	public int getMinimo() {
-		return minimo;
-	}
-
-
-
-	public void setMinimo(int minimo) {
-		this.minimo = minimo;
-	}
-
-
-
-	public int getCritico() {
-		return critico;
-	}
-
-
-
-	public void setCritico(int critico) {
-		this.critico = critico;
-	}
-
-
-
-	public String getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
-
-	public int getCantidad() {
-		return cantidad;
-	}
-
-	public void setCantidad(int cantidad) {
-		this.cantidad = cantidad;
-	}
-	
-	
-	
-	
-
 }
